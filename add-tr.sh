eb27c1f82854270d85de4b6b4019517a2b6ee889
lesta-1 <82885098+lesta-1@users.noreply.github.com>
2021-07-10 02:12:44 +0700

    Update add-tr.sh

--- add-tr.sh
+++ add-tr.sh
@@ -2,9 +2,6 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
-uuid=$(cat /etc/trojan/uuid.txt)
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
